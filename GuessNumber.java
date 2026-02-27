cf9fbf17cc78b0e529b052329f7a6b81d88b384e
markogonzalez-tech <marko.gonzalez.tech@gmail.com>
2026-02-27 20:15:50 +0100

    GuessNumber.java

---
+++ GuessNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int numberToGuess = random.nextInt(100) + 1;
+        int userGuess = 0;
+        int attempts = 0;
+
+        System.out.println("Guess a number between 1 and 100");
+
+        while (userGuess != numberToGuess) {
+
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+            attempts++;
+
+            if (userGuess < numberToGuess) {
+                System.out.println("Too low!");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high!");
+            } else {
+                System.out.println("Correct! Attempts: " + attempts);
+            }
+        }
+
+        scanner.close();
+    }
+}
